571c7477f9841883d7f3bce4de3ea99b7f5cffdd
Xinyi Chen <45192244+summersorange@users.noreply.github.com>
2021-09-07 21:53:51 +0800

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,13 @@
+package first;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0;i<args.length;i++)
+		{
+			System.out.println(args[i]);
+		}
+	}
+
+}
