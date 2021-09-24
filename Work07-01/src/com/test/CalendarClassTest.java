3f9ece4cd4934910cefdde24cb9754f8bf20cca7
”summersrange“ <xchen57@kent.edu>
2021-09-24 14:12:05 +0800

    1.java

---
+++ Work07-01/src/com/test/CalendarClassTest.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import java.util.Calendar;
+
+public class CalendarClassTest {
+	public static void main(String[] args) {
+	Calendar gc=Calendar.getInstance();
+	System.out.println(gc.getClass().getName());
+	}
+}
